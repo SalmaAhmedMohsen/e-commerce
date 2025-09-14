156d19924b3c15171dfcbf0cee3c7ae01125c846
Salma Ahmed <salmamohsen34@gmail.com>
2025-09-14 23:07:29 +0300

    moved the base URL to environment.ts and removed the useless attributes from product interface

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,22 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductsService {
+  private baseUrl = environment.baseUrl
   constructor(private _httpClient: HttpClient) {}
   getProducts(): Observable<any> {
-    return this._httpClient.get('https://dummyjson.com/products?limit=194');
+    return this._httpClient.get(`${this.baseUrl}/products?limit=194`);
   }
   getProductDetails(id: string): Observable<any> {
-    return this._httpClient.get(`https://dummyjson.com/products/${id}`);
+    return this._httpClient.get(`${this.baseUrl}/products/${id}`);
   }
   getCustomersFeedback(): Observable<any>{
-  return this._httpClient.get('https://dummyjson.com/quotes')
+  return this._httpClient.get(`${this.baseUrl}/quotes`)
   }
   getProductsByCategory(category: string): Observable<any> {
-    return this._httpClient.get(`https://dummyjson.com/products/category/${category}`)
+    return this._httpClient.get(`${this.baseUrl}/products/category/${category}`)
   }
 }
